49878b4fc7a476591512be224281160b7f45a41c
DevYameen <yammirza793@gmail.com>
2024-09-21 08:29:44 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,75 +13,87 @@ const Home = () => {
   const navigate = useNavigate()
   const location = useLocation()
 
-  console.log('user',user)
-  const fetchUserDetails = async()=>{
+  // Fetch user details from backend
+  const fetchUserDetails = async () => {
     try {
-        const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
-        const response = await axios({
-          url : URL,
-          withCredentials : true
-        })
+      const URL = `${process.env.REACT_APP_BACKEND_URL}/api/user-details`
+      const response = await axios({
+        url: URL,
+        withCredentials: true,
+      })
 
-        dispatch(setUser(response.data.data))
+      dispatch(setUser(response.data.data))
 
-        if(response.data.data.logout){
-            dispatch(logout())
-            navigate("/email")
-        }
-        console.log("current user Details",response)
+      if (response.data.data.logout) {
+        dispatch(logout())
+        navigate("/email")
+      }
+
+      console.log("Current user details", response.data.data)
     } catch (error) {
-        console.log("error",error)
+      console.error("Error fetching user details:", error)
     }
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchUserDetails()
-  },[])
+  }, [])
 
-  /***socket connection */
-  useEffect(()=>{
-    const socketConnection = io(process.env.REACT_APP_BACKEND_URL,{
-      auth : {
-        token : localStorage.getItem('token')
+  // WebSocket connection
+  useEffect(() => {
+    const socketConnection = io(process.env.REACT_APP_BACKEND_URL, {
+      auth: {
+        token: localStorage.getItem('token'),  // Ensure token is valid
       },
+      transports: ['websocket'],  // Use WebSocket transport
+      reconnectionAttempts: 5,    // Number of reconnection attempts
+      reconnectionDelay: 5000,    // Delay between reconnections
     })
 
-    socketConnection.on('onlineUser',(data)=>{
-      console.log(data)
+    socketConnection.on('connect', () => {
+      console.log("Socket connected:", socketConnection.id)
+    })
+
+    socketConnection.on('connect_error', (error) => {
+      console.error("WebSocket connection error:", error)
+    })
+
+    socketConnection.on('onlineUser', (data) => {
+      console.log("Online users:", data)
       dispatch(setOnlineUser(data))
     })
 
+    // Save socket connection to Redux
     dispatch(setSocketConnection(socketConnection))
 
-    return ()=>{
+    // Clean up on unmount
+    return () => {
+      console.log("Socket disconnected")
       socketConnection.disconnect()
     }
-  },[])
-
+  }, [])
 
   const basePath = location.pathname === '/'
+
   return (
     <div className='grid lg:grid-cols-[300px,1fr] h-screen max-h-screen'>
-        <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
-           <Sidebar/>
-        </section>
+      {/* Sidebar Section */}
+      <section className={`bg-white ${!basePath && "hidden"} lg:block`}>
+        <Sidebar />
+      </section>
 
-        {/**message component**/}
-        <section className={`${basePath && "hidden"}`} >
-            <Outlet/>
-        </section>
+      {/* Message component */}
+      <section className={`${basePath && "hidden"}`}>
+        <Outlet />
+      </section>
 
-
-        <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex" }`}>
-            <div>
-              <img
-                src={logo}
-                width={250}
-                alt='logo'
-              />
-            </div>
-            <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+      {/* Welcome screen if no user is selected */}
+      <div className={`justify-center items-center flex-col gap-2 hidden ${!basePath ? "hidden" : "lg:flex"}`}>
+        <div>
+          <img src={logo} width={250} alt='logo' />
         </div>
+        <p className='text-lg mt-2 text-slate-500'>Select user to send message</p>
+      </div>
     </div>
   )
 }
